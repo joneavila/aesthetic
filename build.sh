13c821f3dc7fb27554f2f9fc9131e1fd5d0748cc
Jonathan Avila <31858207+joneavila@users.noreply.github.com>
2025-03-19 07:47:27 -0600

    Refactor: Update build script documentation

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Usage: ./build.sh [--clean] [<PRIVATE_KEY_PATH>] [<DEVICE_IP>]
-# If PRIVATE_KEY_PATH and DEVICE_IP are provided, the archive will be uploaded to the device with `scp`
-# If --clean is provided, the script will first delete specified files on the device
+# Packages the application into a .muxupd file
+#
+# If PRIVATE_KEY_PATH and DEVICE_IP are provided, the archive will be transferred to the handheld
+# If --clean is provided, the script will delete application files from the handheld left over from previous builds
 
 CLEAN=false
 PRIVATE_KEY_PATH=""
 DEVICE_IP=""
 
-# Process arguments
 for arg in "$@"; do
     if [ "$arg" == "--clean" ]; then
         CLEAN=true
@@ -20,10 +21,10 @@ done
 
 APPLICATION_DIR=mnt/mmc/MUOS/application/Aesthetic
 GLYPH_DIR=opt/muos/default/MUOS/theme/active/glyph/muxapp
-ZIP_BASE_NAME=Aesthetic
+ARCHIVE_BASE_NAME=Aesthetic
 
-# Files to delete when --clean is used
-FILES_TO_DELETE=(
+# Items to delete with --clean
+ITEMS_TO_DELETE=(
     "/.local/share/love/Aesthetic"
     "/mnt/mmc/MUOS/application/Aesthetic"
     "/run/muos/storage/theme/Aesthetic.muxthm"
@@ -33,23 +34,22 @@ FILES_TO_DELETE=(
     "/opt/muos/default/MUOS/theme/active/glyph/muxapp/aesthetic.png"
 )
 
-# Get version from version.lua
+# Get version from src/version.lua
 MAJOR=$(awk '/version.major =/ {print $3}' src/version.lua)
 MINOR=$(awk '/version.minor =/ {print $3}' src/version.lua)
 PATCH=$(awk '/version.patch =/ {print $3}' src/version.lua)
 VERSION="v${MAJOR}.${MINOR}.${PATCH}"
 
-# Clean files on device if requested
 if [ "$CLEAN" = true ]; then
     if [ -z "$PRIVATE_KEY_PATH" ] || [ -z "$DEVICE_IP" ]; then
         echo "Error: --clean requires both PRIVATE_KEY_PATH and DEVICE_IP"
         exit 1
     fi
     
-    echo "Cleaning files on device ${DEVICE_IP}..."
+    echo "Cleaning..."
     
     # Execute delete commands one by one to better handle wildcards
-    for file in "${FILES_TO_DELETE[@]}"; do
+    for file in "${ITEMS_TO_DELETE[@]}"; do
         # Remove leading ./ if present
         remote_file="${file#./}"
         
@@ -72,12 +72,12 @@ rsync -a mux_launch.sh .build/"${APPLICATION_DIR}"
 rsync -a bin/ .build/"${APPLICATION_DIR}"
 rsync -a lib/ .build/"${APPLICATION_DIR}"
 rsync -a src/ .build/"${APPLICATION_DIR}"
-rsync -a src/template/glyph/muxapp/aesthetic.png .build/"${GLYPH_DIR}/aesthetic.png"
+rsync -a src/template/glyph/muxapp/aesthetic.png .build/"${GLYPH_DIR}"
 
-# Create archive, exclude .DS_Store files
-(cd .build && zip -9r "../.dist/${ZIP_BASE_NAME}_${VERSION}.muxupd" * -x "*.DS_Store")
+# Create archive, exclude macOS system files
+(cd .build && zip -9r "../.dist/${ARCHIVE_BASE_NAME}_${VERSION}.muxupd" * -x "*.DS_Store" -x "._*")
 
-# Clean up build directory
+# Delete temporary build directory
 rm -rf .build
 
 if [ -z "$PRIVATE_KEY_PATH" ]; then
@@ -88,7 +88,7 @@ elif [ -z "$DEVICE_IP" ]; then
     exit 0
 else
     echo "Uploading to $DEVICE_IP"
-    scp -i "${PRIVATE_KEY_PATH}" .dist/"${ZIP_BASE_NAME}_${VERSION}.muxupd" root@"${DEVICE_IP}":/mnt/mmc/ARCHIVE
+    scp -i "${PRIVATE_KEY_PATH}" .dist/"${ARCHIVE_BASE_NAME}_${VERSION}.muxupd" root@"${DEVICE_IP}":/mnt/mmc/ARCHIVE
 fi
 
 echo "Done!"
