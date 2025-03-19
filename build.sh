4473fce23ebfc8e653c08b9e8ee1eda0dd8460a8
Jonathan Avila <31858207+joneavila@users.noreply.github.com>
2025-03-18 21:27:55 -0600

    Fix build script; Add --clean option

--- build.sh
+++ build.sh
@@ -1,19 +1,68 @@
 #!/bin/bash
-# Usage: ./build.sh [<PRIVATE_KEY_PATH>] [<DEVICE_IP>]
+# Usage: ./build.sh [--clean] [<PRIVATE_KEY_PATH>] [<DEVICE_IP>]
 # If PRIVATE_KEY_PATH and DEVICE_IP are provided, the archive will be uploaded to the device with `scp`
+# If --clean is provided, the script will first delete specified files on the device
 
-PRIVATE_KEY_PATH=$1
-DEVICE_IP=$2
+CLEAN=false
+PRIVATE_KEY_PATH=""
+DEVICE_IP=""
+
+# Process arguments
+for arg in "$@"; do
+    if [ "$arg" == "--clean" ]; then
+        CLEAN=true
+    elif [ -z "$PRIVATE_KEY_PATH" ]; then
+        PRIVATE_KEY_PATH="$arg"
+    elif [ -z "$DEVICE_IP" ]; then
+        DEVICE_IP="$arg"
+    fi
+done
 
 APPLICATION_DIR=mnt/mmc/MUOS/application/Aesthetic
 GLYPH_DIR=opt/muos/default/MUOS/theme/active/glyph/muxapp
 ZIP_BASE_NAME=Aesthetic
+
+# Files to delete when --clean is used
+FILES_TO_DELETE=(
+    "/.local/share/love/Aesthetic"
+    "/mnt/mmc/MUOS/application/Aesthetic"
+    "/run/muos/storage/theme/Aesthetic.muxthm"
+    "/mnt/sdcard/MUOS/theme/active/glyph/muxapp/aesthetic.png"
+    "/mnt/mmc/ARCHIVE/Aesthetic_*.muxupd"
+    "/mnt/mmc/MUOS/update/installed/Aesthetic_*.muxupd.done"
+    "/opt/muos/default/MUOS/theme/active/glyph/muxapp/aesthetic.png"
+)
 
 # Get version from version.lua
 MAJOR=$(awk '/version.major =/ {print $3}' src/version.lua)
 MINOR=$(awk '/version.minor =/ {print $3}' src/version.lua)
 PATCH=$(awk '/version.patch =/ {print $3}' src/version.lua)
 VERSION="v${MAJOR}.${MINOR}.${PATCH}"
+
+# Clean files on device if requested
+if [ "$CLEAN" = true ]; then
+    if [ -z "$PRIVATE_KEY_PATH" ] || [ -z "$DEVICE_IP" ]; then
+        echo "Error: --clean requires both PRIVATE_KEY_PATH and DEVICE_IP"
+        exit 1
+    fi
+    
+    echo "Cleaning files on device ${DEVICE_IP}..."
+    
+    # Execute delete commands one by one to better handle wildcards
+    for file in "${FILES_TO_DELETE[@]}"; do
+        # Remove leading ./ if present
+        remote_file="${file#./}"
+        
+        # Handle files with wildcards differently
+        if [[ "$remote_file" == *"*"* ]]; then
+            ssh -i "${PRIVATE_KEY_PATH}" root@"${DEVICE_IP}" "for f in ${remote_file}; do if [ -e \"\$f\" ]; then echo \"Found: \$f\"; rm -rf \"\$f\" && echo \"Deleted: \$f\"; else echo \"No matching files for pattern: ${remote_file}\"; fi; done"
+        else
+            ssh -i "${PRIVATE_KEY_PATH}" root@"${DEVICE_IP}" "if [ -e '${remote_file}' ]; then rm -rf '${remote_file}' && echo 'Deleted: ${remote_file}'; else echo 'Not found: ${remote_file}'; fi"
+        fi
+    done
+    
+    echo "Clean completed."
+fi
 
 mkdir -p .dist
 mkdir -p .build/"${APPLICATION_DIR}"
