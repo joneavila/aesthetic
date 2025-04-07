379146eb4939d6953063abaeaa1e0dd83a517ad4
Jonathan Avila <31858207+joneavila@users.noreply.github.com>
2025-04-06 23:26:37 -0600

    Update launch script documentation

--- mux_launch.sh
+++ mux_launch.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 # HELP: Aesthetic is an on-device theme creator.
 # ICON: aesthetic
+# GRID: Aesthetic
 
 # Source muOS system functions
-. /opt/muos/script/var/func.sh
-. -n /opt/muos/script/package/theme.sh
+. /opt/muos/script/var/func.sh # For `GET_VAR`, `SET_VAR`
+. -n /opt/muos/script/package/theme.sh # For `theme.sh` `install` function
 
 # Define paths
 ROOT_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/application/Aesthetic/.aesthetic"
 BINDIR="$ROOT_DIR/bin"
 LOG_DIR="$ROOT_DIR/logs"
 
-# Create required directories
-# Most directories are created by the application
-mkdir -p "$LOG_DIR"
-
 # Export environment variables
 export SDL_GAMECONTROLLERCONFIG_FILE="/usr/lib/gamecontrollerdb.txt"
 export LOG_DIR
-export LD_LIBRARY_PATH="$ROOT_DIR/lib:$ROOT_DIR/tove:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$ROOT_DIR/lib:$ROOT_DIR/tove:$LD_LIBRARY_PATH" # Add libraries to the library path
 export TEMPLATE_DIR="$ROOT_DIR/template"
 
 # Launch application
