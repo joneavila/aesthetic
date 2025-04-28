2548a5a93a364c3ec7e76eb6cd3c9b781b2bc423
Jonathan Avila <31858207+joneavila@users.noreply.github.com>
2025-04-28 12:57:52 -0600

    Update conf.lua documentation

--- src/conf.lua
+++ src/conf.lua
@@ -1,11 +1,12 @@
---- LÖVE configuration file: https://www.love2d.org/wiki/Config_Files
+--- LÖVE configuration file
 local love = require("love")
 
 function love.conf(t)
-	-- These dimensions match most muOS Ambernic supported devices: https://muos.dev/devices/anbernic
+	-- The window dimensions below are set to match most Anbernic devices supported by muOS
+	-- (see: https://muos.dev/devices/anbernic).
+	-- The UI is fully responsive, so `window.resizable` to `true` will allow muOS to resize the window as needed.
 	t.window.width = 640
 	t.window.height = 480
-
 	t.window.resizable = true
 	t.window.fullscreen = false
 	t.window.title = "Aesthetic"
