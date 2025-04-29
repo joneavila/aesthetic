8084e0f59d34785c1fbe96b6079a24c57c76ab9c
Jonathan Avila <31858207+joneavila@users.noreply.github.com>
2025-04-29 06:50:09 -0600

    Update LÖVE configuration: Add window settings, disable unused modules

--- src/conf.lua
+++ src/conf.lua
@@ -4,12 +4,26 @@ local love = require("love")
 function love.conf(t)
 	-- The window dimensions below are set to match most Anbernic devices supported by muOS
 	-- (see: https://muos.dev/devices/anbernic).
-	-- The UI is fully responsive, so `window.resizable` to `true` will allow muOS to resize the window as needed.
+	-- The UI is fully responsive, so `window.resizable` is set to `true` to allow muOS to resize the window
+	-- based on the device's screen dimensions
 	t.window.width = 640
 	t.window.height = 480
+	t.window.minwidth = 640
+	t.window.minheight = 480
 	t.window.resizable = true
+
+	t.window.borderless = false -- Enable for better screenshots
 	t.window.fullscreen = false
+	t.window.highdpi = true
+	t.window.msaa = 4 -- Enable multi-sample anti-aliasing for better quality (at cost of performance)
 	t.window.title = "Aesthetic"
-	t.window.borderless = true
+	t.version = "11.5"
+	t.accelerometerjoystick = false
+
+	-- Enable gamma correction (when supported) for better color accuracy
 	t.gammacorrect = true
+
+	-- Disable unused modules to (slightly) improve performance
+	t.modules.mouse = false
+	t.modules.touch = false
 end
