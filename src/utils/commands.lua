eb61addee7907f9df2c9441621815df36a002368
Jonathan Avila <31858207+joneavila@users.noreply.github.com>
2025-05-10 21:54:00 -0600

    Refactor: Capture error when executing commands

--- src/utils/commands.lua
+++ src/utils/commands.lua
@@ -1,5 +1,4 @@
 --- Executes a command and sets an error message if the command fails
---- This function calls `errorHandler.setError()` so it does not need to be called separately
 
 local errorHandler = require("error_handler")
 local logger = require("utils.logger")
@@ -7,18 +6,32 @@ local logger = require("utils.logger")
 local commands = {}
 function commands.executeCommand(command)
 	logger.debug("Executing command: " .. command)
-	local result = os.execute(command)
+
+	-- Use popen to capture command output
+	local handle = io.popen(command .. " 2>&1")
+	if not handle then
+		logger.error("Failed to execute command: " .. command)
+		errorHandler.setError("Failed to execute command: " .. command)
+		return false
+	end
+
+	local output = handle:read("*a")
+	local _, result, code = handle:close()
+
 	if not result then
 		logger.error("Command failed: " .. command)
 		errorHandler.setError("Failed to execute command: " .. command)
 		return false
 	end
-	if result == 0 then
-		logger.debug("Command executed successfully, result: " .. tostring(result))
+
+	if code == 0 then
+		logger.debug("Command executed successfully, result: " .. tostring(code))
 	else
-		logger.error("Command returned error: " .. tostring(result))
+		logger.error("Command returned error code: " .. tostring(code))
+		logger.error("Command output: " .. output)
 	end
-	return result
+
+	return code
 end
 
 return commands
