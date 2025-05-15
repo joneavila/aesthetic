687eb8c2f02de987f3eec563e7d6d86b1c09c130
Jonathan Avila <31858207+joneavila@users.noreply.github.com>
2025-05-15 10:46:03 -0600

    Update convert_font_to_binary.sh to format output filename

--- utils/convert_font_to_binary.sh
+++ utils/convert_font_to_binary.sh
@@ -3,7 +3,7 @@
 # This script converts a font file to binary format using lv_font_conv.
 # lv_font_conv must be installed and accessible in PATH.
 # The converted font is saved in the same directory as the input font file, with the same name, e.g.,
-# ./fonts/comic-sans.ttf -> ./fonts/comic-sans.bin
+# ./fonts/comic-sans.ttf -> ./fonts/comic_sans_24.bin
 #
 # Usage:
 #   ./convert_font_to_binary.sh <path-to-font-file> [size]
@@ -17,7 +17,7 @@
 #
 # Output:
 #   
-#   Example: ./fonts/roboto.ttf -> ./fonts/roboto.bin
+#   Example: ./fonts/roboto.ttf -> ./fonts/roboto_24.bin
 
 if [ $# -lt 1 ]; then
     echo "Usage: $0 <path-to-font-file> [size]"
@@ -36,8 +36,11 @@ size=${2:-24}
 
 dir_path=$(dirname "$font_path")
 filename=$(basename "$font_path")
+filename_no_ext="${filename%.*}"
 
-output_path="${dir_path}/${filename%.*}.bin"
+# Format the filename: lowercase, replace dashes with underscores, append size
+formatted_filename=$(echo "$filename_no_ext" | tr '[:upper:]' '[:lower:]' | tr '-' '_')
+output_path="${dir_path}/${formatted_filename}_${size}.bin"
 
 # See: https://muos.dev/themes/fonts.html#using-the-script
 lv_font_conv --bpp 4 --size "$size" --font "$font_path" -r 0x00-0xFF --format bin --no-compress --no-prefilter -o "$output_path"
