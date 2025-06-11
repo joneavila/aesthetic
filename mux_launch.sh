6d609d10b53787b72fb816f337c1f8c3c3b300fa
Jonathan Avila <31858207+joneavila@users.noreply.github.com>
2025-06-11 01:25:59 -0600

    Refactor: mux_launch script

--- mux_launch.sh
+++ mux_launch.sh
@@ -9,33 +9,37 @@
 echo "app" >/tmp/act_go
 
 # Define paths
-# `$(GET_VAR "device" "storage/rom/mount")` will resolve to either `/mnt/mmc` (SD1) or `/mnt/sdcard` (SD2)
-ROOT_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/application/Aesthetic"
+ROOT_DIR="/mnt/mmc/MUOS/application/Aesthetic"
 SOURCE_DIR="$ROOT_DIR/.aesthetic"
+USERDATA_DIR="$ROOT_DIR/userdata"
+
 LOG_DIR="$SOURCE_DIR/logs"
+SESSION_LOG_FILE="$LOG_DIR/$(date +%Y%m%d_%H%M%S).log"
+
+LIB_DIR="$SOURCE_DIR/lib"
+TOVE_DIR="$SOURCE_DIR/tove"
 
 # Make sure the directory exists
 mkdir -p "$LOG_DIR"
-mkdir -p "$ROOT_DIR/userdata"
+mkdir -p "$USERDATA_DIR"
 
-# Generate a unique session ID based on timestamp
-SESSION_LOG_FILE="$LOG_DIR/$(date +%Y%m%d_%H%M%S).log"
-
-WIDTH=$(GET_VAR device mux/width)
-HEIGHT=$(GET_VAR device mux/height)
-
-# Export environment variables
+export WIDTH=$(GET_VAR device mux/width)
+export HEIGHT=$(GET_VAR device mux/height)
 export ROOT_DIR
 export SOURCE_DIR
 export SDL_GAMECONTROLLERCONFIG_FILE="/usr/lib/gamecontrollerdb.txt"
 export SESSION_LOG_FILE
-export LD_LIBRARY_PATH="$SOURCE_DIR/lib:$SOURCE_DIR/tove:$LD_LIBRARY_PATH" # Add libraries to the library path
-export WIDTH
-export HEIGHT
+export LD_LIBRARY_PATH="$LIB_DIR:$TOVE_DIR:$LD_LIBRARY_PATH"
 
 # Launch application
 cd "$SOURCE_DIR" || exit
 SET_VAR "system" "foreground_process" "love"
 ./bin/love .
 
-# TODO: `unset` environment variables
+unset WIDTH
+unset HEIGHT
+unset ROOT_DIR
+unset SOURCE_DIR
+unset SDL_GAMECONTROLLERCONFIG_FILE
+unset SESSION_LOG_FILE
+unset LD_LIBRARY_PATH
